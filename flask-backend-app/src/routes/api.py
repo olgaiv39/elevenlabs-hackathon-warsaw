0ecd5b1a0445c02ea634d2eccca99366d6db8a0f
Paweł <p4vv37@gmail.com>
2025-02-22 16:34:30 +0100

    Iterationon tts endpoint

--- flask-backend-app/src/routes/api.py
+++ flask-backend-app/src/routes/api.py
@@ -12,21 +12,26 @@ def generate_story():
 
 @api_bp.route('/tts', methods=['POST'])
 def generate_audio():
-    print(1)
     data = request.json
     text = data.get('text')
     if not text:
         return jsonify({'error': 'Text is required'}), 400
     client = ElevenLabs()
     audio = client.text_to_speech.convert(
-        text="The first move is what sets everything in motion.",
+        text=text,
         voice_id="JBFqnCBsd6RMkjVDRZzb",
-        model_id="eleven_multilingual_v2",
-        output_format="mp3_44100_128",
+        model_id="eleven_multilingual_v2"
     )
 
     play(audio)
-    return jsonify({'audio_url': audio_url}), 200
+    # Save the audio bytes to a file
+    file_path = "output.mp3"
+    audio_bytes = b"".join(audio)
+
+    with open(file_path, "wb") as audio_file:
+        audio_file.write(audio_bytes)
+
+    return jsonify({'audio_file': file_path}), 200
 
 def set_routes(app):
     app.register_blueprint(api_bp, url_prefix='/api')
